ba942968cb5360ef9ac9ebbf2df3bbed586a1f33
takeo828 <takeo.i828@gmail.com>
2023-01-28 23:38:42 +0900

    lesson3課題　再提出

--- src/Review01.java
+++ src/Review01.java
@@ -3,15 +3,15 @@ public class Review01 {
 
     public static void main(String[] args) {
         int price = 1500; // 商品の値段を代入
-        int tax = 10;// 消費税率を代入
+        int taxrate = 10;// 消費税率を代入
         int result;
 
-        result = taxMethod(price , tax);
+        result = tax(price , taxrate);
 
         System.out.println(price + "円の商品の税込価格は" + (price + result) + "円（消費税は" + result + "円）です。");
     }
 
-    public static int taxMethod(int num1, int num2) {
+    public static int tax(int num1, int num2) {
         int result = num1 * num2/100;
         return result;
     }
